4f47df92901ce6b0ab57064b96fabac068fba954
Jean-Francois_Vachon <1333135@cegepba.qc.ca>
2022-09-06 11:32:25 -0400

    Sub

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import RPi.GPIO as GPIO
+import dht11
+import time
+import paho.mqtt.subscribe as subscribe
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+while True:
+    temp = subscribe.simple("sensor/temperature", hostname="10.4.1.127")
+    humidity = subscribe.simple("sensor/humidity", hostname="10.4.1.127")
+    print("%s %s" % (temp.topic, temp.payload))
+    print("%s %s" % (humidity.topic, humidity.payload))
+    time.sleep(5)
+
+
+
